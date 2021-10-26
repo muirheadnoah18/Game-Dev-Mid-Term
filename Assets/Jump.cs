14808cad86bc9ee5f019a3c4dd790f6e872e6cc5
Colby Smith <colby5486@gmail.com>
2021-10-26 13:08:16 -0400

    Fixed Jump

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    public float jumpForce;
+    public Rigidbody reggiebody;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector3 forceVector = Vector3.up * jumpForce;
+            reggiebody.AddForce(forceVector);
+        }
     }
 }
